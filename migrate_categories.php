d69e44df64e87d1bf0d421c2df5ae8f411b2d969
Brett Fitzgerald <brettgfitzgerald@gmail.com>
2013-05-23 16:39:29 -0400

    Organize the categories into a hierarchy - not working yet

--- migrate_categories.php
+++ migrate_categories.php
@@ -34,20 +34,20 @@ try {
   $dbh = new PDO("mysql:host=" . SRC_DB_HOST . ";dbname=". SRC_DB_NAME, SRC_DB_USER, SRC_DB_PW); 
   $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
 
-  $result = $dbh->query('SELECT * from bvc_Category');  
-  $result->setFetchMode( PDO::FETCH_OBJ );
+  $result = $dbh->query('SELECT * from bvc_Category');
 } catch(PDOException $e) {  
   echo $e->getMessage();
   exit();
 }
 
 $category = array();
+$bv_category = array();
 $new_records = 0;
 $skipped_records = 0;
 // Create all the categories in a non-hiearchy. Store the IDs in the DB for later use
 // and store them in an array for use later in this code.
-while($row = $result->fetch()) {
-
+while($row = $result->fetchObject()) {
+  $bv_category[$row->bvin] = $row;
   // Check if we already imported this Bvin
   if(!checkBvinExists($row->bvin, 'bv_x_magento_categories', $mag_dbh)){
 
@@ -72,7 +72,8 @@ while($row = $result->fetch()) {
     // Add record to Array
     $category[] = array(
       'bvin' => $row->bvin,
-      'mag_id' => $id
+      'mag_id' => $id,
+      'name' => $row->Name
     );
 
     $new_records++;
@@ -100,9 +101,31 @@ if( !empty($category)){
 echo "New categories: " . $new_records . "<br>";
 echo "Skipped categories: " . $skipped_records . "<br>";
 
-
 // Iterate through the categories again, modifying the Magento categories to include
 // the hierarchy.
+
+$bvin_to_mag = array();
+$mag_to_bvin = array();
+
+// First, get the ID mappings
+$sql = "SELECT * FROM bv_x_magento_categories";
+$result = $mag_dbh->query($sql);
+
+while($row = $result->fetchObject()){
+  $bvin_to_mag[$row->bvin] = $row->mag_id;
+  $mag_to_bvin[$row->mag_id] = $row->bvin;
+}
+
+//Loop through our new entries and update the Categories using the Magento API
+foreach($bvin_to_mag as $bvin => $mag_id){
+  $result = $dbh->query('SELECT * from bvc_Category WHERE bvin = \'' . $bvin . '\'');
+  if($row = $result->fetchObject()){
+    if($row->ParentID != 0){
+      //$mag_parent = $bvin_to_mag[$row->ParentID];
+      $return = $client->catalogCategoryMove($session, $mag_id, $bvin_to_mag[$row->ParentID]);
+    }
+  }
+}
 
 
 $mag_dbh = null;
