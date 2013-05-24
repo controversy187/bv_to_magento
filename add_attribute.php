7455a850e01c94d81a482b8799c4b6d180209a7b
Brett Fitzgerald <brettgfitzgerald@gmail.com>
2013-05-24 16:59:17 -0400

    Get basic framework for Attributes built

---
+++ add_attribute.php
@@ -0,0 +1,49 @@
+<?php
+include( 'config.php' );
+include( 'api_functions.php' );
+include( 'custom_functions.php' );
+
+$bvin = $_POST['bvin'];
+
+// Establish connection to Magento DB
+try {
+  # MySQL with PDO_MYSQL  
+  $mag_dbh = new PDO("mysql:host=" . MAG_DB_HOST . ";dbname=". MAG_DB_NAME, MAG_DB_USER, MAG_DB_PW); 
+  $mag_dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+}  
+catch(PDOException $e) {  
+  echo $e->getMessage();
+  exit();
+}
+
+
+// Get BV Data
+try {
+  # MySQL with PDO_MYSQL  
+  $dbh = new PDO("mysql:host=" . SRC_DB_HOST . ";dbname=". SRC_DB_NAME, SRC_DB_USER, SRC_DB_PW); 
+  $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+
+  $select_property = $dbh->prepare( "SELECT * FROM bvc_ProductProperty WHERE `bvin` = :bvin_id" );
+  $select_property->bindParam(':bvin_id', $bvin);
+  $select_property->execute();
+} catch(PDOException $e) {  
+  echo $e->getMessage();
+  exit();
+}
+
+if($row = $select_property->fetchObject()){
+  
+  // Check if we already imported this Bvin
+  if(!checkBvinExists($row->bvin, 'bv_x_magento_attributes', $mag_dbh)){
+    echo "<pre>";var_dump($row);echo("</pre>");
+  } else {
+    echo "Record already added";
+  }
+} else {
+  echo "Error: bvin $bvin not found";
+}
+
+
+$mag_dbh = null;
+$dbh = null;
+?>
