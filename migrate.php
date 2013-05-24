f5369ac0d73547a54584c178e815ef65dea9b3b8
Brett Fitzgerald <brettgfitzgerald@gmail.com>
2013-05-24 15:00:11 -0400

    Update navigation

--- migrate.php
+++ migrate.php
@@ -1,2 +1,3 @@
 1. <a href="migrate_categories.php" target="_blank">Import Categories</a><br>
 2. <a href="migrate_categories_hierarchy.php" target="_blank">Organize Categories Into Hierarchy</a><br>
+3. <a href="migrate_attributes.php" target="_blank">Import Attributes</a> (Admin controlled properties of products)<br>
