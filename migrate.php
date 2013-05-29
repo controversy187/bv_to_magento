206019e6b1e7b5e5f3a8a7c5ff112c7ea9e96ef1
Brett Fitzgerald <brettgfitzgerald@gmail.com>
2013-05-29 15:14:09 -0400

    Importing Attribute sets

--- migrate.php
+++ migrate.php
@@ -1,3 +1,4 @@
 1. <a href="migrate_categories.php" target="_blank">Import Categories</a><br>
 2. <a href="migrate_categories_hierarchy.php" target="_blank">Organize Categories Into Hierarchy</a><br>
+2. <a href="migrate_attribute_sets.php" target="_blank">Create Attriubte Sets</a> (Groups for Attributes)<br>
 3. <a href="migrate_attributes.php" target="_blank">Import Attributes</a> (Admin controlled properties of products)<br>
