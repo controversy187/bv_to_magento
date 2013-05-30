c16134d14ce96f51775618e0178a4d06693387ce
Brett Fitzgerald <brettgfitzgerald@gmail.com>
2013-05-30 10:44:56 -0400

    Updated main menu for adding attributes to sets

--- migrate.php
+++ migrate.php
@@ -2,3 +2,4 @@
 2. <a href="migrate_categories_hierarchy.php" target="_blank">Organize Categories Into Hierarchy</a><br>
 3. <a href="migrate_attribute_sets.php" target="_blank">Create Attriubte Sets</a> (Groups for Attributes)<br>
 4. <a href="migrate_attributes.php" target="_blank">Import Attributes</a> (Admin controlled properties of products)<br>
+5. <a href="assign_attributes_to_sets.php" target="_blank">Assign Attributes to Sets</a> (Admin controlled properties of products)<br>
