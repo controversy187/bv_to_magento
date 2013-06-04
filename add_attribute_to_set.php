b136c68dd837de3db33b48f8dbd40e3821601783
Brett Fitzgerald <brettgfitzgerald@gmail.com>
2013-06-04 13:48:23 -0400

    Attributes now adding to attribute sets

--- add_attribute_to_set.php
+++ add_attribute_to_set.php
@@ -6,8 +6,6 @@ include( 'custom_functions.php' );
 $bvin = $_POST['bvin'];
 $set_bvin = $_POST['set_bvin'];
 
-echo "<pre>";var_dump($bvin, $set_bvin);die("</pre>");
-
 // Establish connection to Magento DB
 try {
   # MySQL with PDO_MYSQL  
@@ -20,98 +18,20 @@ catch(PDOException $e) {
 }
 
 
-// Get BV Data
-try {
-  # MySQL with PDO_MYSQL  
-  $dbh = new PDO("mysql:host=" . SRC_DB_HOST . ";dbname=". SRC_DB_NAME, SRC_DB_USER, SRC_DB_PW); 
-  $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+$attribute_id = bvin_to_mag('bv_x_magento_attributes', $bvin, $mag_dbh);
+$set_id = bvin_to_mag('bv_x_magento_attribute_sets', $set_bvin, $mag_dbh);
 
-  $select_property = $dbh->prepare( "SELECT * FROM bvc_ProductProperty WHERE `bvin` = :bvin_id" );
-  $select_property->bindParam(':bvin_id', $bvin);
-  $select_property->execute();
-} catch(PDOException $e) {  
-  echo $e->getMessage();
-  exit();
-}
+$return = $client->catalogProductAttributeSetAttributeAdd(
+    $session,
+    $attribute_id,
+    $set_id
+);
 
-if($row = $select_property->fetchObject()){
-  
-  // Check if we already imported this Bvin
-  if(!checkBvinExists($row->bvin, 'bv_x_magento_attributes', $mag_dbh)){
-    // Convert BV Type Codes to Magento Frontend Input Types.
-    switch ($row->TypeCode) {
-      case '1':
-        $frontend_input = 'text';
-        break;
-      case '2':
-        $frontend_input = 'select';
-        break;
-      case '3':
-        $frontend_input = 'price';
-        break;
-      case '4':
-        $frontend_input = 'date';
-        break;
-      default:
-        $frontend_input = 'text';
-        break;
-    }
-
-    // Catch (some) illegal characters for the Property Name
-    $PropertyName = $row->PropertyName;
-    if($row->PropertyName == ""){
-      $PropertyName = $row->bvin;
-    }
-    $PropertyName = str_replace(' ', '_', $PropertyName);
-    $PropertyName = str_replace('(', '_', $PropertyName);
-    $PropertyName = str_replace(')', '_', $PropertyName);
-    $PropertyName = str_replace(':', '_', $PropertyName);
-    $PropertyName = str_replace('-', '_', $PropertyName);
-    if (!preg_match('/^[a-z]/i', $PropertyName)) { // Make sure it starts with a letter.
-      $PropertyName = 'a' . $PropertyName;
-    }
-    $PropertyName = strtolower($PropertyName);
-    $PropertyName = substr ( $PropertyName, 0, 29 ); // Make sure it is under 30 characters in length
-   
-    $data = array(
-      "attribute_code" => $PropertyName,
-      "frontend_input" => $frontend_input,
-      "scope" => "global",
-      "default_value" => "1",
-      "is_unique" => 0,
-      "is_required" => 0,
-      "apply_to" => array(),
-      "is_configurable" => 0,
-      "is_searchable" => 1,
-      "is_visible_in_advanced_search" => 1,
-      "is_comparable" => 1,
-      "is_used_for_promo_rules" => 1,
-      "is_visible_on_front" => 1,
-      "used_in_product_listing" => 1,
-      "additional_fields" => array(),
-      "frontend_label" => array(array("store_id" => "0", "label" => $row->DisplayName))
-    );
-    
-    $id = $client->catalogProductAttributeCreate($session, $data);
-
-    $sql = "INSERT INTO bv_x_magento_attributes (`bvin`, `mag_id`) VALUES ( '" . $row->bvin . "', " . $id ." );";
-    try{
-      $mag_dbh->query($sql);
-    } catch(PDOException $e) {  
-      echo $e->getMessage();
-      exit();
-    }
-
-    echo "Magento Attribute ID: " . $id;
-
-  } else {
-    echo "Record already added";
-  }
+if($return) {
+  echo "Success - Attribute $attribute_id is a part of set $set_id";
 } else {
-  echo "Error: bvin $bvin not found";
+  echo "ERROR: Attribute $bvin -> $attribute_id, Set $set_bvin -> $set_id";
 }
-
 
 $mag_dbh = null;
-$dbh = null;
 ?>
--- custom_functions.php
+++ custom_functions.php
@@ -8,4 +8,29 @@ function checkBvinExists($bvin, $table, $dbh){
 	return $dbh->query("SELECT count(*) FROM $table WHERE `bvin` = '$bvin'")->fetchColumn() > 0;
 }
 
+/**
+ * Looks up a Magento id from a table that maps Bvins to Magento IDs
+ * @param  String $table Name of the table that contains the mappings
+ * @param  String $bvin  The Bvin
+ * @param  PDO $dbh   The PDO Database connection
+ * @return Int        The Magento ID
+ */
+function bvin_to_mag($table, $bvin, $dbh){
+	try {
+	  # MySQL with PDO_MYSQL  
+	  $magento_id = $dbh->prepare( "SELECT * FROM `" . mysql_real_escape_string($table) . "` WHERE `bvin` = :bvin_id" );
+	  $magento_id->bindParam(':bvin_id', $bvin);
+	  $magento_id->execute();
+	} catch(PDOException $e) {  
+	  echo $e->getMessage();
+	  exit();
+	}
+
+	if($response = $magento_id->fetchObject()){
+    return $response->mag_id;
+  } else {
+  	return false;
+  }
+	
+}
 ?>
