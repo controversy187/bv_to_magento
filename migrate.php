ea5b7ed971eb93f81513e987ca0d029a68face55
Brett Fitzgerald <brettgfitzgerald@gmail.com>
2013-06-07 11:14:53 -0400

    Start framework for adding options

--- migrate.php
+++ migrate.php
@@ -4,3 +4,4 @@
 4. <a href="migrate_attributes.php" target="_blank">Import Attributes</a> (Admin controlled properties of products)<br>
 5. <a href="assign_attributes_to_sets.php" target="_blank">Assign Attributes to Sets</a> (Admin controlled properties of products)<br>
 6. <a href="migrate_products.php" target="_blank">Create Products</a><br>
+7. <a href="migrate_options.php" target="_blank">Create Product Custom Options</a><br>
