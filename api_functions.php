0790f04f315bcb140251691b4dec21f2e7b25bb6
Peter <ppostma@rbc.org>
2013-06-14 13:58:57 -0400

    exhaustive soap initialization

--- api_functions.php
+++ api_functions.php
@@ -2,7 +2,12 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-$client = new SoapClient( SOAP_URL );
+$max = 4;
+do {
+  $client = new SoapClient( SOAP_URL );
+  $max --;
+} while(!$client && $max > 0);
+
 
 // If some stuff requires api authentification, then get a session token
 $session = $client->login( SOAP_LOGIN, SOAP_API_KEY );
