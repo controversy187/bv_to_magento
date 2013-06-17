98e9245d5d47332f410f17619f14f602280ed73a
Brett Fitzgerald <brettgfitzgerald@gmail.com>
2013-06-17 14:18:54 -0400

    If a SKU exists, add it to the website instead of erroring out.

--- add_product.php
+++ add_product.php
@@ -39,6 +39,7 @@ if($row = $select_category->fetchObject()){
     //echo "<pre>";var_dump($row);die("</pre>");
     
     // Get magento set ID from BV ProductTypeID
+    $name             = iconv ( "windows-1252" , "UTF-8" , $row->ProductName );
     $attribute_set_id = bvinToMag('bv_x_magento_attribute_sets', $row->ProductTypeId, $mag_dbh);
     $category_bvin    = getBVCategoryFromProductBvin($row->bvin, $dbh);
     $category_id      = bvinToMag('bv_x_magento_categories', $category_bvin, $mag_dbh);
@@ -49,27 +50,49 @@ if($row = $select_category->fetchObject()){
     $longDesc         = ($row->LongDescription == "" ? iconv ( "windows-1252" , "UTF-8" , $row->ShortDescription ) : iconv ( "windows-1252" , "UTF-8" , $row->LongDescription ));
     //If we don't have a short description, use a truncated long description. That looks messy.
     $shortDesc        = ($row->ShortDescription == "" ? (strlen($longDesc) > 125 ? substr($longDesc, 0, 125) . "... " : substr($longDesc, 0, 125)) : iconv ( "windows-1252" , "UTF-8" , $row->ShortDescription ));
-//    echo "<pre>";var_dump($row);echo("</pre>");
 
-    $id = $client->catalogProductCreate($session, 'simple', $attribute_set_id, $row->SKU, array(
-        'categories' => array($category_id),
-        'websites' => array(1),
-        'name' => iconv ( "windows-1252" , "UTF-8" , $row->ProductName ),
-        'description' => $longDesc,
-        'short_description' => $shortDesc,
-        'weight' => $row->ShippingWeight,
-        'status' => $status,
-        //'url_key' => 'product-url-key',
-        //'url_path' => 'product-url-path',
-        //'visibility' => '4',
-        'price' => $row->SitePrice,
-        'tax_class_id' => $tax_class ,
-        'meta_title' => $meta_title,
-        'meta_keyword' => iconv ( "windows-1252" , "UTF-8" , $row->MetaKeywords ),
-        'meta_description' => iconv ( "windows-1252" , "UTF-8" , $row->MetaDescription )
-      ),
-      STORE_CODE
+    $dataArray = array(
+      'categories' => array($category_id),
+      'websites' => array(WEBSITE_ID),
+      'name' => $name,
+      'description' => $longDesc,
+      'short_description' => $shortDesc,
+      'weight' => $row->ShippingWeight,
+      'status' => $status,
+      //'url_key' => 'product-url-key',
+      //'url_path' => 'product-url-path',
+      //'visibility' => '4',
+      'price' => $row->SitePrice,
+      'tax_class_id' => $tax_class ,
+      'meta_title' => $meta_title,
+      'meta_keyword' => iconv ( "windows-1252" , "UTF-8" , $row->MetaKeywords ),
+      'meta_description' => iconv ( "windows-1252" , "UTF-8" , $row->MetaDescription )
     );
+
+    try{
+      $id = $client->catalogProductCreate($session, 'simple', $attribute_set_id, $row->SKU, $dataArray, STORE_CODE );
+    } catch (SoapFault $e) {
+      if($e->faultstring == 'The value of attribute "SKU" must be unique'){  // Need to add the sku to the store
+        //Get websites from old product and add our new website
+        $result = $client->catalogProductInfo($session, $row->SKU . ' ', STORE_CODE);
+        $id = $result->product_id;
+        $websites = $result->websites;
+        $websites[] = WEBSITE_ID;
+        
+        //Add this site to old product
+        $result = $client->catalogProductUpdate($session, $row->SKU . ' ', array('websites' => $websites));
+        
+        //Update this product with new information
+        unset($dataArray['websites']); // Only deal with our specific Store, don't update all websites
+        $result = $client->catalogProductUpdate($session, $row->SKU . ' ', $dataArray, STORE_CODE);
+
+        echo "Existing product added to store - SKU: " . $row->SKU . " - " . $name . ' - ';
+      } else{
+        echo "<pre>";var_dump($e->faultstring);die("</pre>");
+      }
+    } 
+
+    
 
     $sql = "INSERT INTO bv_x_magento_products (`bvin`, `mag_id`) VALUES ( '" . $row->bvin . "', " . $id ." );";
     try{
@@ -78,7 +101,7 @@ if($row = $select_category->fetchObject()){
       echo $e->getMessage();
       exit();
     }
-    echo "Magento Product ID: " . $id;
+    if($id) echo "Magento Product ID: " . $id;
   } else {
     echo "Record already added";
   }
