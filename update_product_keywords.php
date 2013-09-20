2d4b3355395b61f097f0651340adf68f90700528
Brett Fitzgerald <brettgfitzgerald@gmail.com>
2013-09-20 10:19:54 -0400

    Cat the current Magento Meta Keywords with BV Keywords

---
+++ update_product_keywords.php
@@ -0,0 +1,54 @@
+<?php
+include( 'config.php' );
+include( 'custom_functions.php' );
+$startTime = time();
+
+// Get BV Data
+try {
+  # MySQL with PDO_MYSQL  
+  $dbh = new PDO("mysql:host=" . SRC_DB_HOST . ";dbname=". SRC_DB_NAME, SRC_DB_USER, SRC_DB_PW); 
+  $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+
+  $bv_request = $dbh->prepare( "SELECT * FROM `bvc_Product` WHERE `bvin` = :bvin_id" );
+  $bv_request->bindParam(':bvin_id', $_POST['bvin']);
+  $bv_request->execute();
+
+  $bv_data = $bv_request->fetchObject();
+} catch(PDOException $e) {  
+  echo $e->getMessage();
+  exit();
+}
+
+//echo "<pre>";var_dump($bv_data);die("</pre>");
+$sku = $bv_data->SKU;
+
+include( 'api_functions.php' );
+    
+try{
+  $result = $client->catalogProductInfo($session, $sku . ' ', STORE_CODE);
+} catch (SoapFault $e) {
+  echo "<pre>";var_dump($e);die("</pre>");
+}
+
+$keywords = $result->meta_keyword . ', ' . $bv_data->Keywords;
+$name 		= $result->name;
+
+$dataArray = array(
+  'meta_keyword'   => $keywords
+);
+
+//echo "<pre>";var_dump($dataArray);die("</pre>");
+
+//Update the products
+try{
+  //$result = $client->catalogProductUpdate($session, $sku . ' ', $dataArray, STORE_CODE);  
+} catch (SoapFault $e){
+  echo "<pre>";var_dump($e);die("</pre>");
+}
+
+$timePassed = time() - $startTime;
+echo " Magento Product '" . $sku . "' - New Keywords: " . $keywords , " - (" . $timePassed . " seconds total)";
+
+$mag_dbh = null;
+$dbh = null;
+?>
---
+++ update_products_keywords.php
@@ -0,0 +1,88 @@
+<?php
+include( 'config.php' );
+include( 'custom_functions.php' );
+
+// Establish connection to Magento DB
+try {
+  # MySQL with PDO_MYSQL  
+  $mag_dbh = new PDO("mysql:host=" . MAG_DB_HOST . ";dbname=". MAG_DB_NAME, MAG_DB_USER, MAG_DB_PW); 
+  $mag_dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+
+  //Check if the table exists, create it if it doesn't
+  if(!checkTable('bv_x_magento_products_keywords', $mag_dbh)){
+    $result = $mag_dbh->query('
+      CREATE TABLE `bv_x_magento_products` (
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `bvin` varchar(45) DEFAULT NULL,
+        `mag_id` int(10) DEFAULT NULL,
+        PRIMARY KEY (`id`),
+        UNIQUE KEY `bvin_UNIQUE` (`bvin`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+    ');
+  }
+}  
+catch(PDOException $e) {  
+  echo $e->getMessage();
+  exit();
+}
+
+
+// Get BV Data
+try {
+  # MySQL with PDO_MYSQL  
+  $dbh = new PDO("mysql:host=" . SRC_DB_HOST . ";dbname=". SRC_DB_NAME, SRC_DB_USER, SRC_DB_PW); 
+  $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+
+  $result = $dbh->query('SELECT * from bvc_Product');
+} catch(PDOException $e) {  
+  echo $e->getMessage();
+  exit();
+}
+?>
+
+<html>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" ></script>
+<script>
+var bvins = new Array();
+
+<?php
+// Create all the categories in a non-hiearchy. Store the IDs in the DB for later use
+// and store them in an array for use later in this code.
+
+while($row = $result->fetchObject()) echo "bvins.push('" . $row->bvin . "');\n";
+
+$mag_dbh = null;
+$dbh = null;
+?>
+$(document).ready(function(){
+
+  totalBvins = bvins.length;
+  $('#responseBlock1').append('Adding ' + totalBvins + ' bvins<br>');
+
+  //totalBvins = 10; // Delete this when going live. Only limit to 10 bvins for development
+  addProductKeywords(bvins[0], 0, totalBvins);
+});
+
+function addProductKeywords(bvin_id, iteration, max){
+  humanNumber = iteration+1;
+  $('#responseBlock1').append('<br>' + humanNumber + ' / ' + max + ' : ' + bvin_id + '... ');
+
+  $.ajax({
+    url: "update_product_keywords.php",
+    type: "POST",
+    data: {bvin : bvin_id},
+    dataType: "html"
+  }).done(function(msg, status) {
+    $('#responseBlock1').append(status + " - " + msg );
+    if(iteration+1 < max){
+      addProductKeywords(bvins[iteration+1], iteration+1, max);
+    }
+  });
+}
+</script>
+<body>
+<pre>
+<div id="responseBlock1"></div>
+</pre>
+</body>
+</html>
